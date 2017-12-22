813c861ad05916263f5228baeff83b33b8579283
Feng Guo <feng.guo2001@gmail.com>
2017-12-22 14:03:41 -0500

    Added more lists to add and new move subclass

---
+++ TeachersPet/src/teacherspet/Battling/ListOfCharacters.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+class ListOfCharacters {
+    ArrayList<Character> characterArrayList;
+
+    ListOfCharacters() {
+        //characterArrayList.add(new PlayableCharacter());
+    }
+}
