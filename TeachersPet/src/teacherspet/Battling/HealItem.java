68049401e173d54891ff6bb49b07050d98a49700
sihansu <34429570+sihansu@users.noreply.github.com>
2018-01-05 19:45:33 -0500

    fixed multipliers, added starbucks card, updated heal items, finished inventory class with 4 methods

--- TeachersPet/src/teacherspet/Battling/HealItem.java
+++ TeachersPet/src/teacherspet/Battling/HealItem.java
@@ -1,11 +1,16 @@
 class HealItem extends Item {
+    private String type;
     private int change;
 
-    HealItem(String name, int change) {
+    HealItem(String name, String type, int change) {
         super(name);
+        this.type = type;
         this.change = change;
     }
 
+    public String getType() {
+        return type;
+    }
     public int getChange(){
         return change;
     }
--- TeachersPet/src/teacherspet/Battling/ListOfInventoryItems.java
+++ TeachersPet/src/teacherspet/Battling/ListOfInventoryItems.java
@@ -6,24 +6,31 @@ public class ListOfInventoryItems {
     ListOfInventoryItems() {
         itemList = new ArrayList<Item>();
         //stat item - held
-        itemList.add(new StatItem("Glasses", "Held","Intelligence",0.1));
-        itemList.add(new StatItem("Dumbbells", "Held","Attack",0.1));
-        itemList.add(new StatItem("Notes", "Held","Defense",0.1));
-        itemList.add(new StatItem("Calculator", "Held","Intelligence",0.1));
+        itemList.add(new StatItem("Glasses", "Held","Intelligence",1.1));
+        itemList.add(new StatItem("Dumbbells", "Held","Attack",1.1));
+        itemList.add(new StatItem("Notes", "Held","Defense",1.1));
+        itemList.add(new StatItem("Calculator", "Held","Intelligence",1.1));
+        itemList.add(new StatItem("Starbuck's card", "Held", "Health", 1.06));
         //stat item - clothing
-        itemList.add(new StatItem("Jordans", "Clothing","Speed",0.1));
-        itemList.add(new StatItem("Yeezys", "Clothing","Speed",0.15));
-        itemList.add(new StatItem("Headband", "Clothing","Intelligence",0.1));
-        itemList.add(new StatItem("Headphones", "Clothing","Health",0.1));
-        itemList.add(new StatItem("Stussy Sweater", "Clothing","Attack",0.2));
+        itemList.add(new StatItem("Jordans", "Clothing","Speed",1.1));
+        itemList.add(new StatItem("Yeezys", "Clothing","Speed",1.15));
+        itemList.add(new StatItem("Headband", "Clothing","Intelligence",1.1));
+        itemList.add(new StatItem("Headphones", "Clothing","Health",1.1));
+        itemList.add(new StatItem("Stussy Sweater", "Clothing","Attack",1.2));
         //heal item
-        itemList.add(new HealItem("Caf Food", 5));
-        itemList.add(new HealItem("Tim Hortons Coffee", 10));
-        itemList.add(new HealItem("McDonald's Burger", 20));
-        itemList.add(new HealItem("Bubble Tea", 25));
-        itemList.add(new HealItem("Starbucks Frappuccino", 30));
-        itemList.add(new HealItem("Bulk Barn Candy", 40));
-        itemList.add(new HealItem("Ramen", 50));
+        itemList.add(new HealItem("Caf Food", "HP", 5));
+        itemList.add(new HealItem("McDonald's Burger", "HP", 20));
+        itemList.add(new HealItem("Bubble Tea", "HP", 25));
+        itemList.add(new HealItem("Bulk Barn Candy", "HP", 40));
+        itemList.add(new HealItem("Tim Hortons Coffee", "PP", 10));
+        itemList.add(new HealItem("Starbucks Frappuccino", "PP", 100)); //full restore of PP for a certain move
+        itemList.add(new HealItem("Red bull", "Sleep", 0));
+        itemList.add(new HealItem("Ramen", "Burn", 0));
+        itemList.add(new HealItem("Doritos", "Poison", 0));
+        itemList.add(new HealItem("Tea", "Stun", 0));
+        itemList.add(new HealItem("Sushi", "Full restore", 0));
+        itemList.add(new HealItem("Chicken nuggets (20)", "Half revive", 0));
+        itemList.add(new HealItem("Chicken nuggets (50)", "Full revive", 0));
         //capture item
         itemList.add(new CaptureItem("Aced test", 0.8));
         itemList.add(new CaptureItem("Aced quiz", 0.5));
