3eb2b681481ddff5d44d454489b172c0a4197099
Feng Guo <feng.guo2001@gmail.com>
2018-01-06 13:39:22 -0500

    changed initializing characters

--- TeachersPet/src/teacherspet/Battling/ListOfCharacters.java
+++ TeachersPet/src/teacherspet/Battling/ListOfCharacters.java
@@ -3,9 +3,11 @@ import java.util.ArrayList;
 class ListOfCharacters{
     private ArrayList<Character> characterList;
     private ListOfMoves startMoves;
+    private ListOfInventoryItems inventoryItems;
 
     ListOfCharacters() {
         startMoves = new ListOfMoves();
+        inventoryItems = new ListOfInventoryItems();
         characterList = new ArrayList<Character>();
         //Movesets for playable characters
         String[] fengMoveNames = {"Steal Code", "Throw Chair", "Stack Overflow", "Snort Candy"};
@@ -26,15 +28,15 @@ class ListOfCharacters{
         Move[] mrTimmermanMoves = returnMoveArray(mrTimmermanMoveNames);
 
         //List of playable characters
-        characterList.add(new PlayableCharacter(50, 60, 20, 50, 60, "Technology", "Feng", "[description]", fengMoves, "Clown"));
-        characterList.add(new PlayableCharacter(25, 25, 70, 20, 60, "Math", "Joyce", "[description]", joyceMoves, "Persistent"));
-        characterList.add(new PlayableCharacter(20, 20, 65, 60, 35, "English", "Sihan", "[description]", sihanMoves, "Distressed"));
-        characterList.add(new PlayableCharacter(30, 40, 30, 70, 30, "Science", "Yash", "[description]", yashMoves, "Relieve"));
+        characterList.add(new PlayableCharacter(50, 60, 20, 50, 60, "Technology", "Feng", "[description]", fengMoves, "Clown", null, null, null, null, null));
+        characterList.add(new PlayableCharacter(25, 25, 70, 20, 60, "Math", "Joyce", "[description]", joyceMoves, "Persistent", null, null, null, null, null));
+        characterList.add(new PlayableCharacter(20, 20, 65, 60, 35, "English", "Sihan", "[description]", sihanMoves, "Distressed", null, null, null, null, null));
+        characterList.add(new PlayableCharacter(30, 40, 30, 70, 30, "Science", "Yash", "[description]", yashMoves, "Relieve", null, null, null, null, null));
 
         //List of nonplayable characters
-        characterList.add(new NonPlayableCharacter(60, 30, 110, 70, 230, "Math", "Mr. Choi", "perabler", mrChoiMoves, 100, "Speed Boost"));
-        characterList.add(new NonPlayableCharacter(300, 50, 100, 30, 20, "Math", "Mr. Shim", "Let homework be your guide", mrShimMoves, 100, "Demoralize"));
-        characterList.add(new NonPlayableCharacter(200, 40, 100, 60, 100, "Math", "Mr. Timmerman", "Focus isn't just the name of a car.", mrTimmermanMoves, 100, "Unaware"));
+        characterList.add(new NonPlayableCharacter(60, 30, 110, 70, 230, "Math", "Mr. Choi", "perabler", mrChoiMoves, 100, "Speed Boost", null));
+        characterList.add(new NonPlayableCharacter(300, 50, 100, 30, 20, "Math", "Mr. Shim", "Let homework be your guide", mrShimMoves, 100, "Demoralize", null));
+        characterList.add(new NonPlayableCharacter(200, 40, 100, 60, 100, "Math", "Mr. Timmerman", "Focus isn't just the name of a car.", mrTimmermanMoves, 100, "Unaware", null));
     }
 
     private Move[] returnMoveArray(String[] moveNames) {
