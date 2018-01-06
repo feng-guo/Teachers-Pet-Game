711edc60b1403b686005e46beb5e09c424e283c3
Feng Guo <feng.guo2001@gmail.com>
2018-01-06 17:00:01 -0500

    deleted folder

--- src/battling/ProtectMove.java
+++
@@ -1,7 +0,0 @@
-package battling;
-
-class ProtectMove extends Move {
-    ProtectMove(String name, double hitChance, String type, int maxPowerPoints, int priority) {
-        super(name, 0, hitChance, type, "None", maxPowerPoints, priority);
-    }
-}
