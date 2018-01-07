1fb5a1404ef9a5182b107035799c6987494023e0
Feng Guo <feng.guo2001@gmail.com>
2018-01-07 17:01:21 -0500

    dwa

--- src/battle/NonPlayableCharacter.java
+++ src/battle/NonPlayableCharacter.java
@@ -1,6 +1,6 @@
 package battle;
 
-class NonPlayableCharacter extends Character {
+public class NonPlayableCharacter extends Character {
   private String speech; //Something they say when they start a battle
   private boolean defeated; //Tells whether or not the character has been defeated
   private int experience; //Experience the user gains
