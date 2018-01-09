7d466d425cffc920d759fb077127ddeef261330e
joycetruong <joyce.truong591@gmail.com>
2018-01-09 12:46:27 -0500

    Mini game methods for stress eat dropper

--- Mini Games/StressEat/src/stresseat/StressEat.java
+++ Mini Games/StressEat/src/stresseat/StressEat.java
@@ -1,9 +1,16 @@
 package stresseat;
 
-public class StressEat {
+public class StressEat implements Timer, MiniGamePoints {
 
     public static void main(String[] args) {
-        
     }
-    
+
+    // Randomly generate food
+    public static void createFood() {
+    }
+
+    // Drop food
+    public static void movement() {
+    }
+
 }
