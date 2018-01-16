7e9059e3015e6372a246672c9f7901b41bbc9422
sihansu <34429570+sihansu@users.noreply.github.com>
2018-01-16 00:41:10 -0500

    fixed warning bc thread.stop() sucks, still kinda inefficient with the scanner tho

--- src/mini_games/mathcontest/MathContest.java
+++ src/mini_games/mathcontest/MathContest.java
@@ -4,20 +4,24 @@ import java.util.Scanner;
 
 public class MathContest {
     public static void main(String args[]){
-        Clock clock = new Clock();
-        Questions questions = new Questions();
+        Clock clock = new Clock(); //create clock thread
+        Questions questions = new Questions(); //create questions thread
 
+        //intro
         System.out.println("Welcome to the math contest!");
         System.out.println("Are you ready to improve your intelligence?");
+
+        //start questions and clock
         questions.start();
         clock.start();
 
+        //check if clock is finished
         do{
             if(clock.getState()== Thread.State.TERMINATED) {
-                questions.interrupt();
+                // questions.interrupt(); //interrupts questions thread to display final points
                 System.out.println("Thanks for playing!");
-                System.out.println("Your score was " + questions.getPoints());
-                questions.stop();
+                System.out.println("Your score was " + questions.getPoints() + " out of " + questions.getNumQuestions());
+                System.out.println("Your percentage is " + questions.getPercentage() + "%");
             }
         }while(clock.getState() != Thread.State.TERMINATED);
     }
