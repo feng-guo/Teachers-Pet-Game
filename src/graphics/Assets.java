cebbe79ca701f54e446ac6decf55a050109cb9ff
joycetruong <joyce.truong591@gmail.com>
2018-01-17 00:34:07 -0500

    uh

--- src/graphics/Assets.java
+++ src/graphics/Assets.java
@@ -42,9 +42,9 @@ public class Assets {
 			e.printStackTrace();
 		}
 		
-		String characterName = "Misha";
+		String characterName = "Joyce";
 
-		SpriteSheet fengSheet = new SpriteSheet(ImageLoader.loadImage("/Sprite_Images/sprites/characters/" + characterName + "/" + characterName.toLowerCase() + "_spritesheet.png"));
+		SpriteSheet fengSheet = new SpriteSheet(ImageLoader.loadImage ("/Sprite_Images/sprites/characters/" + characterName + "/" + characterName.toLowerCase() + "_spritesheet.png"));
 		SpriteSheet tileSheet = new SpriteSheet(ImageLoader.loadImage("/tile_sheet.png"));
 		SpriteSheet grassSheet = new SpriteSheet(ImageLoader.loadImage("/tile_textures.png"));
 		SpriteSheet floorSheet = new SpriteSheet(ImageLoader.loadImage("/sample4.png"));
