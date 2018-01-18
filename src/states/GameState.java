e6cab27205623d78bd51950341455aec66184427
joycetruong <joyce.truong591@gmail.com>
2018-01-18 12:09:27 -0500

    shit

--- src/states/GameState.java
+++ src/states/GameState.java
@@ -18,8 +18,16 @@ public class GameState extends State{
 		super(handler);
 
 		//world = new World(handler, "res/worlds/world1.txt");
-		world = new World(handler, "res/worlds/world2.txt");
+		//world = new World(handler, "res/worlds/world2.txt");
 		//world = new World(handler, "res/worlds/cafeteria.txt");
+		//world = new World(handler, "res/worlds/guidance.txt");
+
+		//world = new World(handler, "res/worlds/compsci.txt");
+		//world = new World(handler, "res/worlds/drama.txt");
+		//world = new World(handler, "res/worlds/english.txt");
+		//world = new World(handler, "res/worlds/gym.txt");
+		//world = new World(handler, "res/worlds/math.txt");
+		//world = new World(handler, "res/worlds/science.txt");
 
 		handler.setWorld(world);
 				
---
+++ src/tiles/GymTile.java
@@ -0,0 +1,13 @@
+package tiles;
+
+import java.awt.image.BufferedImage;
+
+import graphics.Assets;
+
+public class GymTile extends Tile{
+
+    public GymTile(int id) {
+        super(Assets.gymTile, id);
+    }
+
+}
