8f8064fb40af313cf8a2eea0a768f41db27f4b56
Yash <yash_arora6@hotmail.com>
2018-01-18 02:35:57 -0500

    saffwgw

--- src/battle/BattleRunner.java
+++ src/battle/BattleRunner.java
@@ -37,8 +37,8 @@ public class BattleRunner {
         newSquad[2] = (PlayableCharacter)characterList.returnCharacter("Sihan");
         newSquad[3] = (PlayableCharacter)characterList.returnCharacter("Yash");
         newSquad[4] = (PlayableCharacter)characterList.returnCharacter("Johann");
-        newSquad[5] = (PlayableCharacter)characterList.returnCharacter("Misha");
-        //newSquad[5] = (PlayableCharacter)characterList.returnCharacter("Angela");
+        //newSquad[5] = (PlayableCharacter)characterList.returnCharacter("Misha");
+        newSquad[5] = (PlayableCharacter)characterList.returnCharacter("Angela");
         MrChoi = (NonPlayableCharacter)characterList.returnCharacter("Mr.Choi");
         MrShim = (NonPlayableCharacter)characterList.returnCharacter("Mr.Shim");
         MrTimmerman = (NonPlayableCharacter)characterList.returnCharacter("Mr.Timmerman");
@@ -51,7 +51,7 @@ public class BattleRunner {
         inventory.addItem(inventoryItems.retrieveItem("Caf Food"));
         inventory.addItem(inventoryItems.retrieveItem("Caf Food"));
         inventory.addItem(inventoryItems.retrieveItem("Caf Food"));
-
+    }
 
     public void startRandomBattle() {
         double random2 = Math.random();
