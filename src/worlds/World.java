2722472327af2681ec3a5db859f77692c84e1e17
joycetruong <joyce.truong591@gmail.com>
2018-01-18 23:29:37 -0500

    oops

--- src/worlds/World.java
+++ src/worlds/World.java
@@ -232,7 +232,6 @@ public class World {
 	
 	public void loadBottomHall() {
 
-	    /*
 		entityManager.addEntity(new NPC(handler, Assets.angela_down, Assets.angela_up, Assets.angela_left, Assets.angela_right, 100, 250, 100));
 		entityManager.addEntity(new NPC(handler, Assets.feng_down, Assets.feng_up, Assets.feng_left, Assets.feng_right, 100, 400, 100));
 		entityManager.addEntity(new NPC(handler, Assets.bill_down, Assets.bill_up, Assets.bill_left, Assets.bill_right, 100, 550, 100));
@@ -241,7 +240,6 @@ public class World {
 		entityManager.addEntity(new NPC(handler, Assets.sihan_down, Assets.sihan_up, Assets.sihan_left, Assets.sihan_right, 100, 1000, 100));
 		entityManager.addEntity(new NPC(handler, Assets.misha_down, Assets.misha_up, Assets.misha_left, Assets.misha_right, 100, 1140, 440));
 		entityManager.addEntity(new NPC(handler, Assets.johann_down, Assets.johann_up, Assets.johann_left, Assets.johann_right, 100, 1300, 100));
-        */
 
 	    //HALLWAY
 		//Walls and windows
