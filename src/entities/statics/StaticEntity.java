c1497d5c6256ffa7f945c1a590693a38fef06659
joycetruong <joyce.truong591@gmail.com>
2018-01-19 12:38:32 -0500

    yes

--- src/entities/statics/StaticEntity.java
+++ src/entities/statics/StaticEntity.java
@@ -5,8 +5,12 @@ import game.Handler;
 
 import java.awt.image.BufferedImage;
 
+/* Class to make objects that will be placed in the map
+ * inherited by specific objects
+ */
 public abstract class StaticEntity extends Entity{
 
+	// The constructor for each object with its bounds and the creator
 	public StaticEntity(Handler handler, float x, float y, int width, int height) {
 		super(handler, x, y, width, height);
 	}
