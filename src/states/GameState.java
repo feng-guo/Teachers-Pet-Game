41cea06feb279e877186b9cba0fe0491db6112ca
joycetruong <joyce.truong591@gmail.com>
2018-01-18 20:30:12 -0500

    yeet

--- src/states/GameState.java
+++ src/states/GameState.java
@@ -16,6 +16,10 @@ public class GameState extends State{
 	public GameState(Handler handler) {
 		super(handler);
 
+		world1 = new World(handler, "res/worlds/world1.txt");
+		world2 = new World(handler, "res/worlds/world2.txt");
+
+		cafeteria = new World(handler, "res/worlds/cafeteria.txt");
 		guidance = new World(handler, "res/worlds/guidance.txt");
 
 		compsci = new World(handler, "res/worlds/compsci.txt");
@@ -25,10 +29,6 @@ public class GameState extends State{
 		math = new World(handler, "res/worlds/math.txt");
 		science = new World(handler, "res/worlds/science.txt");
 
-		world1 = new World(handler, "res/worlds/world1.txt");
-		world2 = new World(handler, "res/worlds/world2.txt");
-		cafeteria = new World(handler, "res/worlds/cafeteria.txt");
-		
 		world = science;
 		
 		handler.setWorld(world);
@@ -56,7 +56,22 @@ public class GameState extends State{
 			world = world2;
 		} else if (path.equals("res/worlds/cafeteria.txt")) {
 			world = cafeteria;
+		} else if (path.equals("res/worlds/guidance.txt")) {
+			world = guidance;
+		} else if (path.equals("res/worlds/compsci.txt")) {
+			world = compsci;
+		} else if (path.equals("res/worlds/drama.txt")) {
+			world = drama;
+		} else if (path.equals("res/worlds/english.txt")) {
+			world = english;
+		} else if (path.equals("res/worlds/gym.txt")) {
+			world = gym;
+		} else if (path.equals("res/worlds/math.txt")) {
+			world = math;
+		} else {
+			world = science;
 		}
+
 		handler.setWorld(world);
 		
 	}
